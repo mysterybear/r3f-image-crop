e9024ab6ff45bc8b92b66dc49a73decb44a2bfe6
Tom Bear <tom@bearjam.dev>
2021-04-15 21:01:45 +0100

    ^^

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { shaderMaterial } from "@react-three/drei"
+import { extend, useFrame, useLoader } from "@react-three/fiber"
+import { useControls } from "leva"
+import React, { useEffect, useRef } from "react"
+import * as THREE from "three"
+import fragmentShader from "./shaders/fragment.glsl"
+import vertexShader from "./shaders/vertex.glsl"
 
-function App() {
+const FooShaderMaterial = shaderMaterial(
+  {
+    u_image: new THREE.Texture(),
+    u_inset: new THREE.Vector4(0, 0, 0, 0),
+  },
+  vertexShader,
+  fragmentShader
+)
+
+extend({ FooShaderMaterial })
+
+const App = () => {
+  const img = useLoader(
+    THREE.TextureLoader,
+    "https://images.unsplash.com/photo-1613910117442-b7ef140b37f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE4NTAxMzg5&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+  )
+  const ref = useRef()
+
+  useEffect(() => {
+    ref.current.uniforms.u_image.value = img
+  }, [img])
+
+  const { t, r, b, l } = useControls({
+    t: {
+      value: 0.0,
+      step: 0.001,
+      min: 0.0,
+      max: 1.0,
+    },
+    r: {
+      value: 0.0,
+      step: 0.001,
+      min: 0.0,
+      max: 1.0,
+    },
+    b: {
+      value: 0.0,
+      step: 0.001,
+      min: 0.0,
+      max: 1.0,
+    },
+    l: {
+      value: 0.0,
+      step: 0.001,
+      min: 0.0,
+      max: 1.0,
+    },
+  })
+
+  useFrame(() => {
+    ref.current.uniforms.u_inset.value.x = t
+    ref.current.uniforms.u_inset.value.y = r
+    ref.current.uniforms.u_inset.value.z = b
+    ref.current.uniforms.u_inset.value.w = l
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh>
+      <planeBufferGeometry args={[1, 6 / 8]} />
+      <fooShaderMaterial ref={ref} />
+    </mesh>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <Canvas>
+        <Suspense fallback={null}>
+          <App />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+)

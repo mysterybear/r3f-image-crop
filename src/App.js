50c15161045cb52c70c57a1924d4e9ef1b4dde8b
Tom Bear <tom@bearjam.dev>
2021-04-19 13:53:21 +0100

    ^^

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { useSpring } from "@react-spring/core"
 import { shaderMaterial } from "@react-three/drei"
-import { extend, useFrame, useLoader } from "@react-three/fiber"
-import { useControls } from "leva"
-import React, { useEffect, useRef } from "react"
+import { extend, useLoader } from "@react-three/fiber"
+import React from "react"
 import * as THREE from "three"
 import fragmentShader from "./shaders/fragment.glsl"
 import vertexShader from "./shaders/vertex.glsl"
@@ -22,50 +22,20 @@ const App = () => {
     THREE.TextureLoader,
     "https://images.unsplash.com/photo-1613910117442-b7ef140b37f5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE4NTAxMzg5&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
   )
-  const ref = useRef()
 
-  useEffect(() => {
-    ref.current.uniforms.u_image.value = img
-  }, [img])
+  const [{ inset }] = useSpring(() => ({
+    inset: [0.0, 0.0, 0.2, 0.2],
+  })) // doesn't work
 
-  const { t, r, b, l } = useControls({
-    t: {
-      value: 0.0,
-      step: 0.001,
-      min: 0.0,
-      max: 1.0,
-    },
-    r: {
-      value: 0.0,
-      step: 0.001,
-      min: 0.0,
-      max: 1.0,
-    },
-    b: {
-      value: 0.0,
-      step: 0.001,
-      min: 0.0,
-      max: 1.0,
-    },
-    l: {
-      value: 0.0,
-      step: 0.001,
-      min: 0.0,
-      max: 1.0,
-    },
-  })
-
-  useFrame(() => {
-    ref.current.uniforms.u_inset.value.x = t
-    ref.current.uniforms.u_inset.value.y = r
-    ref.current.uniforms.u_inset.value.z = b
-    ref.current.uniforms.u_inset.value.w = l
-  })
+  // const thisInsetWorks = [0.3,0.3,0.3,0.3]
 
   return (
     <mesh>
-      <planeBufferGeometry args={[1, 6 / 8]} />
-      <fooShaderMaterial ref={ref} />
+      <planeBufferGeometry args={[3, 18 / 8]} />
+      <fooShaderMaterial
+        uniforms-u_image-value={img}
+        uniforms-u_inset-value={inset}
+      />
     </mesh>
   )
 }
